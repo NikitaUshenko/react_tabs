c7dd647f1fa5e2fc49b42af28293c9ec70cdf8c2
NikitaUshenko <nikita-ushenko@mail.ru>
2019-07-31 11:36:52 +0300

    Updates

---
+++ precache-manifest.f245005dbca7008947f3fbca61e8f946.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddd2571728d3f4c2d5ce4250d72b2c2f",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "c98d66404b7834f13314",
+    "url": "/react_tabs/static/css/main.8e298290.chunk.css"
+  },
+  {
+    "revision": "f39f7d1fc93e8752222d",
+    "url": "/react_tabs/static/js/2.f33e9c95.chunk.js"
+  },
+  {
+    "revision": "c98d66404b7834f13314",
+    "url": "/react_tabs/static/js/main.a1b2cb7f.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
